791bfa3500268b3c17d08a50a3a578df6e93ef20
MariaOskar <mariaoskarr@gmail.com>
2018-07-05 07:56:39 +0300

    Подготовка проекта к запуску в многопоточном режиме
     - Добавлена кофигурация в pom.xml, отвечающая за запуск в многопоточном режиме.
     - Для корректной работы JDI в многопоточном режиме в test.properties добавлен параметр multithread=true
     - В BaseTest переменная WebDriver driver перенесена в контейнер ThreadLocal<Webdriver> driverContainer

--- src/test/java/com/blazedemo/BaseTest.java
+++ src/test/java/com/blazedemo/BaseTest.java
@@ -11,12 +11,15 @@ import com.epam.jdi.uitests.web.settings.WebSettings;
 import com.epam.jdi.uitests.web.testng.testRunner.TestNGBase;
 import com.sun.javafx.util.Logging;
 import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.otus.utils.PropertyHelper;
 
@@ -26,20 +29,21 @@ import java.util.logging.Level;
 import static com.epam.jdi.uitests.web.settings.WebSettings.useDriver;
 
 public class BaseTest extends TestNGBase {
-    @BeforeSuite(description = "Создание драйвера и инициализация сайта")
+    public static ThreadLocal<WebDriver> driverContainer = new ThreadLocal<>();
+    @BeforeMethod(description = "Создание драйвера и инициализация сайта")
     public static void setUp() throws IOException {
         String flag = PropertyHelper.getProperty("test.properties","test.traffic");
         if(flag!=null && flag.equals("1")){
-            WebDriverUtils.killAllRunWebBrowsers();
 
             LoggingPreferences loggingPreferences = new LoggingPreferences();
             loggingPreferences.enable(LogType.PERFORMANCE, Level.INFO);
 
             ChromeOptions options = new ChromeOptions();
             options.setCapability(CapabilityType.LOGGING_PREFS, loggingPreferences);
-            ChromeDriver chromeDriver = new ChromeDriver(options);
 
-            WebSite.init(useDriver(()->chromeDriver),BlazeDemoSite.class);
+            driverContainer.set(new ChromeDriver(options));
+
+            WebSite.init(useDriver(()->driverContainer.get()),BlazeDemoSite.class);
         }else{
             WebSite.init(BlazeDemoSite.class);
         }
